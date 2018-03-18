f4932add8b954174a026b387d4ae25370b30f8d8
Mark Franceschelli <mark.franceschelli@emc.com>
2018-03-18 15:30:54 -0400

    adding original files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rh-ex1
+Exercise 1 - debug and fix the provided code, and provide a unit test using mocha.js to verify it.
---
+++ remote-math-service.js
@@ -0,0 +1,37 @@
+/*
+ * MF 031818 - original Javascript for remoteMathService exercise 1.
+ */
+
+function remoteMathService(cb) {
+  var one, two;
+
+  callOneService (function(err, num) {
+    one = num;
+  });
+
+  callTwoService(function(err, num) {
+    two = num;
+  });
+  return cb(undefined, one + two);
+}
+
+function callOneService(cb) {
+  setTimeout(function() {
+    return cb(undefined, 1);
+  }, 1000);
+}
+
+function callTwoService(cb) {
+  setTimeout(function() {
+    return cb(undefined, 2);
+  }, 1500);
+}
+
+remoteMathService(function(err, answer) {
+  if (err) console.log("error", err);
+  if (answer !== 3) {
+    console.log("wrong answer", answer);
+  } else {
+    console.log("correct");
+  }
+});
